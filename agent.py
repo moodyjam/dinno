8e08864ef20368dab24a426a173139a69215047a
Jamison Moody <jamisonmmoody@gmail.com>
2024-03-21 15:54:31 -0600

    Working on getting dinno to work

--- agent.py
+++ agent.py
@@ -2,10 +2,19 @@ import torch.nn as nn
 import torch
 
 class Agent(nn.Module):
-     def __init__(self, config, model):
-        super(Agent, self).__init__()
-        self.config = config
-        self.model = model
-        params = torch.nn.utils.parameters_to_vector(model.parameters())
-        self.register_buffer("dual", torch.zeros_like(params))
+   def __init__(self, config, model):
+      super(Agent, self).__init__()
+      self.config = config
+      self.model = model
+      params = torch.nn.utils.parameters_to_vector(model.parameters())
+      self.register_buffer("dual", torch.zeros_like(params))
+      self.neighbor_params = []
+
+   def reset_neighbor_params(self):
+      self.neighbor_params = []
+
+   def append_neighbor_params(self, neighbor_params):
+      self.neighbor_params.append(neighbor_params)
+
+   
         
--- dinno.py
+++ dinno.py
@@ -27,17 +27,15 @@ class ConvNeuralNet(nn.Module):
 
 # define the LightningModule
 class DiNNO(L.LightningModule):
-    def __init__(self, agent_config, graph_type="complete", fiedler_value=None, num_classes=10):
+    def __init__(self, agent_config, graph_type="complete", fiedler_value=None, num_classes=10, B=2, rho=0.5):
         super().__init__()
-        self.graph_type = graph_type
-        self.fiedler_value = fiedler_value
         self.num_nodes = len(agent_config)
         self.G, self.G_connectivity = create_graph(num_nodes = self.num_nodes,
                                                    graph_type = graph_type,
                                                    target_connectivity = fiedler_value)
         
         base_model = ConvNeuralNet(num_classes=num_classes)
-        self.agent_to_idx = {agent["id"]: i for i, agent in enumerate(agent_config)}
+        self.agent_id_to_idx = {agent["id"]: i for i, agent in enumerate(agent_config)}
 
         # Initialize the networks for each agent
         self.models = nn.ModuleDict({agent["id"]: deepcopy(base_model) for agent in agent_config})
@@ -46,17 +44,41 @@ class DiNNO(L.LightningModule):
                                           model=deepcopy(base_model))
                                           for i, agent in enumerate(self.agent_config)}
         
+        self.save_hyperparameters()
+        
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
         # it is independent of forward
 
+        # Reset the variable containing the neighbor parameters
+        for agent_id in self.agents:
+            curr_agent = self.agents[agent_id]
+            curr_agent.reset_neighbor_params()
+
         # Communicate
         for agent_id in self.agents:
-            agent_idx = self.agent_to_idx[agent_id]
+            curr_agent_idx = self.agent_id_to_idx[agent_id]
             curr_agent = self.agents[agent_id]
-            agent_params_vec = torch.nn.utils.parameters_to_vector(curr_agent.model.parameters())
-            for neighbor_idx in self.G.neighbors(agent_idx):
-                print()
+            curr_agent_params = torch.nn.utils.parameters_to_vector(curr_agent.model.parameters())
+            for neighbor_idx in self.G.neighbors(curr_agent_idx):
+                neighbor_id = self.agent_config[neighbor_idx]["id"]
+                self.agents[neighbor_id].append_neighbor_params(curr_agent_params)
+
+        # Technically this can be done in parallel
+        for agent_id in self.agents:
+            curr_agent = self.agents[agent_id]
+            curr_agent_idx = self.agent_id_to_idx[agent_id]
+            theta = torch.nn.utils.parameters_to_vector(curr_agent.model.parameters())
+            neighbor_params = torch.stack(curr_agent.neighbor_params)
+            dual = curr_agent.dual + self.hparams.rho * (theta - neighbor_params).sum(0)
+            primal = theta
+            x, y = batch[curr_agent_idx]
+            for tau in range(self.hparams.B):
+                
+
+
+
+
 
         x, y = batch
         x = x.view(x.size(0), -1)
